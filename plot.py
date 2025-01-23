fdbf752f849e3d611499634d1e715434780276a3
Reddonut-cyber <akhowsuk@gmail.com>
2025-01-23 10:51:03 +0700

    x*sin(2x)

--- plot.py
+++ plot.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x):
-    return np.arctan(x)
+    return x*np.sin(2*x)
 
 x = np.linspace(-100, 100, 1000)
-plt.plot(f(x), color='orange', marker='o', markersize=1, linestyle='dotted')
+plt.plot(f(x), color='orange', marker='o', markersize=1, linestyle='dashed')
 plt.xlabel('x')
 plt.ylabel('f(x)')
-plt.title('f(x) = arctan(x)')
+plt.title('f(x) = x*sin(2x)')
 plt.grid(True)
 plt.legend()
 plt.show()
