9ac9bec40d4b832492097c77612d01021fd68ef0
Reddonut-cyber <akhowsuk@gmail.com>
2025-01-23 10:20:29 +0700

    Update plot function to use sine and change plot color to red

--- plot.py
+++ plot.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x):
-    return x**2
+    return x*np.sin(2*x)
 
 x = np.linspace(-100, 100, 1000)
-plt.plot(f(x), color='green', marker='o', markersize=1)
+plt.plot(f(x), color='red', marker='o', markersize=1)
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid(True)
