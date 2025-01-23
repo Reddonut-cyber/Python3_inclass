589e09998ac228c46871ea551f9fe0f8faba805f
Reddonut-cyber <akhowsuk@gmail.com>
2025-01-23 10:49:19 +0700

    arctan(x)

--- plot.py
+++ plot.py
@@ -5,9 +5,10 @@ def f(x):
     return np.arctan(x)
 
 x = np.linspace(-100, 100, 1000)
-plt.plot(f(x), color='orange', marker='o', markersize=1)
+plt.plot(f(x), color='orange', marker='o', markersize=1, linestyle='dotted')
 plt.xlabel('x')
 plt.ylabel('f(x)')
+plt.title('f(x) = arctan(x)')
 plt.grid(True)
 plt.legend()
 plt.show()
