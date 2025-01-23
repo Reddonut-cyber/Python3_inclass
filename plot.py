955b857509443068501d013bae0c9450cd596685
Reddonut-cyber <akhowsuk@gmail.com>
2025-01-23 10:22:31 +0700

    Update plot function to use arctan and change plot color to orange

--- plot.py
+++ plot.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x):
-    return x*np.sin(2*x)
+    return np.arctan(x)
 
 x = np.linspace(-100, 100, 1000)
-plt.plot(f(x), color='red', marker='o', markersize=1)
+plt.plot(f(x), color='orange', marker='o', markersize=1)
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid(True)
