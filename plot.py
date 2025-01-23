cc16255191d9c8d18dae66fafd653117d41cb470
Reddonut-cyber <akhowsuk@gmail.com>
2025-01-23 10:51:40 +0700

    x*sin(2x) red

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ def f(x):
     return x*np.sin(2*x)
 
 x = np.linspace(-100, 100, 1000)
-plt.plot(f(x), color='orange', marker='o', markersize=1, linestyle='dashed')
+plt.plot(f(x), color='red', marker='o', markersize=1, linestyle='dashed')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.title('f(x) = x*sin(2x)')
